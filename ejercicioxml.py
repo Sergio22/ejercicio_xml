4f7a16f032f6dcc3d560f650581bd5efca0c3892
Sergio <sergiomedranocastro@gmail.com>
2015-02-24 19:55:08 +0100

    esto es una prueba estable de codigo. Ya funciona, ahora solamente queda implementar todo los casos

--- ejercicioxml.py
+++ ejercicioxml.py
@@ -1,5 +1,9 @@
 # -*- encoding: UTF-8 -*-
 from xml.dom import minidom
 xmldoc = minidom.parse("proyecto.xml")
-itemlist = xmldoc.getElementsByTagName("titulo")
-print (len (itemlist))
+itemlist = xmldoc.getElementsByTagName("address_component")
+for items in itemlist:
+	tipo = items.getElementsByTagName("type")[0].firstChild.data
+	if tipo=="administrative_area_level_2":
+		print ("Provincia:")
+		print items.getElementsByTagName("long_name")[0].firstChild.data
